a071edc41e90fec1015704179b875d892ccaee88
Timothy T. Joe <ttjoe0@gmail.com>
2023-10-19 08:22:33 +0000

    fixing require not define in vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import requireTransform from "vite-plugin-require-transform";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), requireTransform({})],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
