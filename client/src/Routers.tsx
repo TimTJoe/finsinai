960dec2baa3624f7fe01cd07b62ca296f9b9ca71
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-04 18:53:16 +0000

    clear folder

--- client/src/Routers.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-
-export const Routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-]);
